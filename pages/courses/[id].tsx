b12e68fd62d99439e722aa9cfea069ac64c8db58
smotastic <nilssmol@gmail.com>
2022-02-01 14:32:15 +0100

    Fix href missing bug

--- pages/courses/[id].tsx
+++ pages/courses/[id].tsx
@@ -1,8 +1,12 @@
+import { Skeleton } from "@mui/material";
 import { useRouter } from "next/router";
 import CourseDetailContainer from "../../src/course-detail/application/course-detail.container";
 
 export default function CourseDetailPage() {
     const router = useRouter();
     const { query } = router;
+    if (!query.id) {
+        return <Skeleton />
+    }
     return <CourseDetailContainer id={query.id as string} />
 }
